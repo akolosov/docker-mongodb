0be6288ba6242a99f209917979725739f812eea7
Alexey Kolosov <alexey.kolosov@gmail.com>
2014-11-20 20:15:45 +0500

    FIX

--- mongodb-startup.sh
+++ mongodb-startup.sh
@@ -1,21 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ -n $MONGODB_SINGLE_SERVER ]; then
+if [ -n "$MONGODB_SINGLE_SERVER" ]; then
 	echo "Starting up single MongoDB server"
   /usr/bin/mongod --config $MONGOD_CONFIG_FILE --dbpath $MONGODB_DATA_PATH --port $MONGODB_MAIN_PORT --notablescan --noprealloc --smallfiles $OPTIONS	 	
 fi
 
-if [ -n $MONGODB_REPLICA_SET ]; then
+if [ -n "$MONGODB_REPLICA_SET" ]; then
 	echo "Starting up MongoDB replica set $MONGODB_REPLICA_SET"
-	/usr/bin/mongod --config $MONGOD_CONFIG_FILE --dbpath $MONGODB_DATA_PATH --port $MONGODB_MAIN_PORT --notablescan --noprealloc --smallfiles --replSet $MONGODB_REPLICA_SET $OPTIONS	 	
+	/usr/bin/mongod --config $MONGOD_CONFIG_FILE --replSet $MONGODB_REPLICA_SET --dbpath $MONGODB_DATA_PATH --port $MONGODB_MAIN_PORT --notablescan --noprealloc --smallfiles $OPTIONS	 	
 fi
 
-if [ -n $MONGODB_CONFIG_SET ]; then
+if [ -n "$MONGODB_CONFIG_SET" ]; then
 	echo "Starting up MongoDB config server $MONGODB_REPLICA_SET"
 	/usr/bin/mongod --config $MONGOD_CFG_CONFIG_FILE --configsvr --dbpath $MONGODB_METADATA_PATH --port $MONGODB_CONFIG_PORT $OPTIONS	
 fi
 
-if [ -n $MONGODB_ROUTER_SET ]; then
+if [ -n "$MONGODB_ROUTER_SET" ]; then
 	echo "Starting up MongoDB router $MONGODB_REPLICA_SET"
 	/usr/bin/mongos --config $MONGOS_CONFIG_FILE --configdb $MONGODB_CONFIG_SERVERS --port $MONGODB_ROUTER_PORT $OPTIONS
 fi

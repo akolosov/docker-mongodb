253aa198568e7edfad8214c496af69d55f793d01
Alexey Kolosov <alexey.kolosov@gmail.com>
2014-11-21 09:42:41 +0500

    FIX

--- mongodb-startup.sh
+++ mongodb-startup.sh
@@ -9,7 +9,8 @@ if [ -n "$MONGODB_REPLICA_SET" ]; then
 	echo "Starting up MongoDB replica set $MONGODB_REPLICA_SET"
 	/usr/bin/mongod --config $MONGOD_CONFIG_FILE --dbpath $MONGODB_DATA_PATH --port $MONGODB_MAIN_PORT --notablescan --noprealloc --smallfiles --replSet $MONGODB_REPLICA_SET $OPTIONS
 	sleep 3
-	/usr/bin/mongo --host 127.0.0.1 --port $MONGODB_MAIN_PORT < /usr/local/etc/initiate.js
+	echo "Setting up MongoDB replica set $MONGODB_REPLICA_SET"
+	cat /usr/local/etc/initiate.js|/usr/bin/mongo --host $HOSTNAME --port $MONGODB_MAIN_PORT
 fi
 
 if [ -n "$MONGODB_CONFIG_SET" ]; then
